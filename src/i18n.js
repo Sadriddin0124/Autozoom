3f273612d607b71d8726525bdf3606a4287ebfd9
Your Name <you@example.com>
2024-06-13 16:03:59 +0500

    first commit

---
+++ src/i18n.js
@@ -0,0 +1,22 @@
+// src/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(HttpApi) // load translations using http (default public/locales)
+  .use(LanguageDetector) // detect user language
+  .use(initReactI18next) // pass the i18n instance to react-i18next.
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
+  });
+
+export default i18n;
---
+++ src/plugins/changeLang.js
@@ -0,0 +1,5 @@
+import i18n from "../i18n";
+
+export const switchLang = (lang) => {
+    i18n.changeLanguage(lang);
+};
